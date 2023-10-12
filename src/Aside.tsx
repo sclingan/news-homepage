e46986a6e1085999e46258fd103c1efcbd616f26
Scott Clingan <sclingan1@gmail.com>
2023-10-12 14:55:28 -0500

    Adding fonts, spacing, padding

--- src/Aside.tsx
+++ src/Aside.tsx
@@ -7,14 +7,20 @@ function Aside() {
         <aside>
             <h3 className='news-header'>New</h3>
 
-            <div>Hydrogen VS Electric Cars
-            Will hydrogen-fueled cars ever catch up to EVs?</div>
+            <div className="div_aside">
+                <h4>Hydrogen VS Electric Cars</h4>
+                <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+                </div>
 
-            <div>The Downsides of AI Artistry
-            What are the possible adverse effects of on-demand AI image generation?</div>
+            <div className="div_aside">
+                <h4>The Downsides of AI Artistry</h4>
+                <p>What are the possible adverse effects of on-demand AI image generation?</p>
+                </div>
 
-            <div>Is VC Funding Drying Up?
-            Private funding by VC firms is down 50% YOY. We take a look at what that means.</div>
+            <div className="div_aside">
+                <h4>Is VC Funding Drying Up?</h4>
+                <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+                </div>
 
         </aside>
     )
