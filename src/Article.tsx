18fbf232bf42c6b3b76bfdd625462b4dd8e7c610
Scott Clingan <sclingan1@gmail.com>
2023-10-30 13:38:57 -0500

    Adding mobile menu functionallity

--- src/Article.tsx
+++ src/Article.tsx
@@ -15,7 +15,7 @@ function Article() {
              <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
              But is it really fulfilling its promise?</p>
 
-             <button>Read more</button>
+             <button className='button-more'>Read more</button>
         </article>
     )
   }
--- src/Header.tsx
+++ src/Header.tsx
@@ -3,13 +3,19 @@ import iconMenu from '../public/images/icon-menu.svg';
 import iconClose from '../public/images/icon-menu-close.svg';
 
 function Header() {
-
+    function close() {
+      let close = document.getElementById('icon-close');
+      let nav = document.getElementById('nav');
+      close?.classList.remove('icon-close');
+      close?.classList.add('hidden');
+      nav?.classList.add('hidden');
+    }
     return (
         <header>
             <img src={logo} alt="a W logo"></img>
             {/* <img src={iconMenu} className='menu-icon' alt='hamburger menu'></img> */}
-            <nav>
-              <img src={iconClose} alt='' className='icon-close'></img>
+            <nav id='nav'>
+              <button className='icon-close' id='icon-close' onClick={close}><img src={iconClose} alt=''></img></button>
               <ul>
                 <li><a href="">Home</a></li>
                 <li><a href="">New</a></li>
